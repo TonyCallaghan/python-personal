92441cc0da63fa531c2e180648f5b4a2d618a776
Tony Callaghan <57832437+TonyCallaghan@users.noreply.github.com>
2022-01-02 13:22:18 +0000

    Update sort.py

--- Sorting and deleting duplicates in a txt file/sort.py
+++ Sorting and deleting duplicates in a txt file/sort.py
@@ -5,12 +5,12 @@ with open (filename) as file_in:
 	for line in file_in:				# For every line in Animals.txt
 		items.append(line.strip()) 		# Add to 'items' list and remove \n character
 
-items = list(dict.fromkeys(items))  	# Remove duplicates
-items.sort()							# Sort it alphabetically
+items = list(dict.fromkeys(items))  			# Remove duplicates
+items.sort()						# Sort it alphabetically
 for i in range(len(items)):			
-    items[i] = items[i].lower()			# Lower case everything
+    items[i] = items[i].lower()				# Lower case everything
 
-filename = 'Nouns/Animals.txt'	# Open new file 
+filename = 'Nouns/Animals.txt'				# Open new file 
 with open (filename, 'w') as file_out:	
 	for item in items:
 		file_out.write(item + '\n')		# Store sorted list in new file
