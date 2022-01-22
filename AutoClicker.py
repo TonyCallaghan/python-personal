d0b42ccb7b5abe35584683c11a7a999704f659c7
Tony Callaghan <57832437+TonyCallaghan@users.noreply.github.com>
2022-01-22 17:31:22 +0000

    NEW WORLD => Comments just for you Spamalot

--- AutoClicker.py
+++ AutoClicker.py
@@ -1,4 +1,6 @@
 # New World resource collection
+# Mainly used for water which sells for 6c a piece
+# 3 hours ~ 3000 coin
 
 import pyautogui
 import time
@@ -7,3 +9,10 @@ x = True
 while x == True: 
 	pyautogui.press('e') # Collect resource
 	time.sleep(4) # 4 second wait time
+	
+# Download Python from windows store or whatever
+# Save this file on notepad
+# Run the code from cmd line
+# Go back to new world and go into the ocean
+# *Put water in all inventory slots*
+# GG => Never worry about money again :)
